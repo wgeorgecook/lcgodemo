8a38cd329f38da1c0b3c57bb86c0d2cea9ef82a0
= <=>
2024-04-18 21:45:36 -0400

    Implement generative search myself I guess

--- internal/vectors/weaviate.go
+++ internal/vectors/weaviate.go
@@ -2,13 +2,14 @@ package vectors
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"lcgodemo/internal/pdf"
 	"lcgodemo/internal/rag"
 	"log"
+	"os"
 
+	"github.com/tmc/langchaingo/llms"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/graphql"
 	"github.com/weaviate/weaviate-go-client/v4/weaviate/schema"
@@ -21,9 +22,7 @@ func GetSchema() (*schema.Dump, error) {
 	if err := CreateSchemaIfNotExists(); err != nil {
 		return nil, err
 	}
-	if err := InsertData(context.Background()); err != nil {
-		return nil, err
-	}
+
 	schema, err := client.Schema().Getter().Do(context.Background())
 	if err != nil {
 		return nil, err
@@ -40,9 +39,9 @@ func InitWeaviate() error {
 	cfg := weaviate.Config{
 		Host:   "weaviate:8080",
 		Scheme: "http",
-		// Headers: map[string]string{
-		// 	"X-OpenAI-Api-Key": os.Getenv("OPENAI_API_KEY"),
-		// },
+		Headers: map[string]string{
+			"X-OpenAI-Api-Key": os.Getenv("OPENAI_API_KEY"),
+		},
 	}
 
 	var err error
@@ -55,6 +54,8 @@ func InitWeaviate() error {
 }
 
 func InsertData(ctx context.Context) error {
+	log.Println("inserting data")
+	defer log.Println("done!")
 	content, err := pdf.ReadFromFile(ctx)
 	if err != nil {
 		return err
@@ -64,19 +65,13 @@ func InsertData(ctx context.Context) error {
 		return err
 	}
 
-	vectorMap := make(map[string]models.Vector)
-	for i, text := range vectors {
-		page := fmt.Sprintf("page %v", i+1)
-		vectorMap[page] = text
-	}
-
 	data := &models.Object{
 		Class: pdfClass,
 		Properties: map[string]any{
 			"title":   "file.pdf",
 			"content": content,
 		},
-		Vectors: vectorMap,
+		Vector: vectors,
 	}
 
 	log.Println("start batch insert")
@@ -102,69 +97,59 @@ func InsertData(ctx context.Context) error {
 	return nil
 }
 
-func GetData(ctx context.Context) (*models.GraphQLResponse, error) {
-	fields := []graphql.Field{
-		{
-			Name: "title",
-		},
-		{
-			Name: "content",
-		},
-	}
-	res, err := client.GraphQL().Get().WithClassName(pdfClass).WithFields(fields...).Do(ctx)
-	if err != nil {
-		return nil, err
-	}
+func QueryData(ctx context.Context, limit int) ([]*models.Object, error) {
 
-	if len(res.Errors) != 0 {
-		for _, err := range res.Errors {
-			log.Printf("query error: %+v", err)
-		}
-
-		return nil, errors.New("too many errors")
-	}
-
-	return res, nil
-}
-func QueryData(ctx context.Context, search string) (*models.GraphQLResponse, error) {
-	fields := []graphql.Field{
-		{
-			Name: "title",
-		},
-		{
-			Name: "content",
-		},
-	}
-
-	queryVector, err := rag.EmbedQuery(ctx, search)
-	if err != nil {
-		return nil, err
-	}
-
-	nearText := client.GraphQL().
-		NearVectorArgBuilder().
-		WithVector(queryVector)
-
-	generativeSearch := graphql.
-		NewGenerativeSearch().
-		SingleResult("Explain {answer} as you might to a five-year-old.")
-
-	result, err := client.GraphQL().Get().
+	result, err := client.Data().ObjectsGetter().
 		WithClassName(pdfClass).
-		WithFields(fields...).
-		WithNearVector(nearText).
-		WithLimit(2).
-		WithGenerativeSearch(generativeSearch).
+		WithLimit(1).
+		WithVector().
 		Do(context.Background())
 	if err != nil {
 		return nil, err
 	}
 
-	jsonOutput, err := json.MarshalIndent(result, "", "  ")
+	return result, nil
+}
+
+func GenerativeSearch(ctx context.Context, search string, llm llms.Model) ([]string, error) {
+	queryVector, err := rag.EmbedQuery(ctx, search)
 	if err != nil {
 		return nil, err
 	}
-	log.Println(string(jsonOutput))
 
-	return result, nil
+	nearVector := client.GraphQL().
+		NearVectorArgBuilder().
+		WithVector(queryVector)
+
+	fields := []graphql.Field{
+		{
+			Name: "content",
+		},
+	}
+
+	results, err := client.GraphQL().Get().
+		WithClassName(pdfClass).
+		WithFields(fields...).
+		WithLimit(1).
+		WithNearVector(nearVector).
+		Do(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	if len(results.Errors) > 0 {
+		return nil, errors.New(results.Errors[0].Message)
+	}
+
+	var generatedPrompts []string
+	for _, res := range results.Data {
+		var prompt = fmt.Sprintf("given the following, summarize the content in a 256 character tweet: %s", res)
+		result, err := llms.GenerateFromSinglePrompt(ctx, llm, prompt)
+		if err != nil {
+			log.Printf("cannot generate prompt: %s", err.Error())
+		}
+		generatedPrompts = append(generatedPrompts, result)
+	}
+
+	return generatedPrompts, nil
 }
